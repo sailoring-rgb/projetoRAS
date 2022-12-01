df14c04b8928c8021db05af6bcdbf2bc4c0e311f
Anaa10 <a93284@alunos.uminho.pt>
2022-12-01 20:35:39 +0000

    não consigo centralizar os botões

--- fase2/frontend/src/components/blocks/Functions.js
+++ fase2/frontend/src/components/blocks/Functions.js
@@ -2,25 +2,25 @@ import "../../css/blocks/Functions.scss";
 import { Link } from "react-router-dom";
 import notificationIcon from "../../imgs/notificationRead.png";
 import NotificationsModalView from "../views/NotificationsModalView";
-//import { FunctionsCard } from "../items/FunctionsCard";
+import userButton from "../../imgs/user_button.png";
 
 export const Functions = ({closeModal }) => {
     const isSelected = (path) => path === window.location.pathname;
     
     return (
             <ul>
-                <li className={isSelected("/profile") ? "selected" : ""}>
+                <button className={isSelected("/profile") ? "selected" : ""}>
                     <Link to={`/functions/profile`}>Perfil</Link>
-                </li>
-                <li className={isSelected("/notificações") ? "selected" : ""}>
+                </button>
+                <button className={isSelected("/notificações") ? "selected" : ""}>
                     <Link to={`/functions/notifications`}>Notificações</Link>
-                </li>
-                <li className={isSelected("/mybets") ? "selected" : ""}>
+                </button>
+                <button className={isSelected("/mybets") ? "selected" : ""}>
                     <Link to={`/functions/mybets`}>Histórico de apostas</Link>
-                </li>
-                <li className={isSelected("/mytransactions") ? "selected" : ""}>
+                </button>
+                <button className={isSelected("/mytransactions") ? "selected" : ""}>
                     <Link to={`/functions/mytransactions`}>Histórico de transações</Link>
-                </li>
+                </button>
             </ul>
     );
 };
