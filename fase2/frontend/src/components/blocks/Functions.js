d866687c5426848c5b8cbbc8b92177d66fa5a837
Ana Henriques <pbapohenriques@hotmail.com>
2022-12-02 10:50:01 +0000

    buttons fixed

--- fase2/frontend/src/components/blocks/Functions.js
+++ fase2/frontend/src/components/blocks/Functions.js
@@ -4,23 +4,31 @@ import notificationIcon from "../../imgs/notificationRead.png";
 import NotificationsModalView from "../views/NotificationsModalView";
 import userButton from "../../imgs/user_button.png";
 
-export const Functions = ({closeModal }) => {
+export const Functions = ({ closeModal }) => {
     const isSelected = (path) => path === window.location.pathname;
-    
+
     return (
-            <ul>
-                <button className={isSelected("/profile") ? "selected" : ""}>
-                    <Link to={`/functions/profile`}>Perfil</Link>
-                </button>
-                <button className={isSelected("/notificações") ? "selected" : ""}>
-                    <Link to={`/functions/notifications`}>Notificações</Link>
-                </button>
-                <button className={isSelected("/mybets") ? "selected" : ""}>
-                    <Link to={`/functions/mybets`}>Histórico de apostas</Link>
-                </button>
-                <button className={isSelected("/mytransactions") ? "selected" : ""}>
-                    <Link to={`/functions/mytransactions`}>Histórico de transações</Link>
-                </button>
-            </ul>
+        <ul>
+            <button className={isSelected("/profile") ? "selected" : ""}>
+                <Link to={`/functions/profile`}>
+                    <p>Perfil</p>
+                </Link>
+            </button>
+            <button className={isSelected("/notificações") ? "selected" : ""}>
+                <Link to={`/functions/notifications`}>
+                    <p>Notificações</p>
+                </Link>
+            </button>
+            <button className={isSelected("/mybets") ? "selected" : ""}>
+                <Link to={`/functions/mybets`}>
+                    <p>Histórico de apostas</p>
+                </Link>
+            </button>
+            <button className={isSelected("/mytransactions") ? "selected" : ""}>
+                <Link to={`/functions/mytransactions`}>
+                    <p>Histórico de transações</p>
+                </Link>
+            </button>
+        </ul>
     );
 };
