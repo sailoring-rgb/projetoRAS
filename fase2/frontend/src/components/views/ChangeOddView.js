c9421b9043818a02590ecb58241d7f8ca470779b
Anaa10 <a93284@alunos.uminho.pt>
2022-12-04 23:50:25 +0000

    pagina do alterar odd

--- fase2/frontend/src/components/views/ChangeOddView.js
+++ fase2/frontend/src/components/views/ChangeOddView.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import { CreateGame} from "../items/CreateGame"
-import { NavBar } from "../blocks/NavBar";
-import { Link, useNavigate, useResolvedPath } from "react-router-dom";
-import '../../css/views/CreateGameView.scss'
+import '../../css/views/ChangeOddView.scss'
 
 export const ChangeOddView = () => {
     const [ gameID, setGameId ] = useState('')
@@ -10,42 +7,37 @@ export const ChangeOddView = () => {
     const [error, setError] = useState("");
     
     return (
-        <div>
-            <div className='nav-container'>
-                <NavBar />
-            </div>
-            <div className='create-game-container'> 
-                <form action="action_page.php">
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="GameID">Game ID:</label>
-                        </div>
-                        <div>
-                            <CreateGame
-                            value={gameID}
-                            type="text"
-                            placeholder="Game ID"
-                            onChange={(e) => [setGameId(e.target.value), setError("")]}
-                            />
-                        </div>
+        <div className='change-odd-container'> 
+            <form action="action_page.php">
+                <div class="row">
+                    <div class="col-25">
+                        <label for="GameID">Game ID:</label>
                     </div>
+                    <div>
+                        <input
+                        value={gameID}
+                        type="text"
+                        placeholder="Game ID"
+                        onChange={(e) => [setGameId(e.target.value), setError("")]}
+                        />
+                    </div>
+                </div>
 
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="odd">Odd do jogo:</label>
-                        </div>
-                        <div>
-                            <CreateGame
-                            value={odd}
-                            type="text"
-                            placeholder="Odd"
-                            onChange={(e) => [setOdd(e.target.value), setError("")]}
-                            />
-                        </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label for="odd">Odd do jogo:</label>
                     </div>
-                </form>
-                <button>Adicionar</button>
-            </div>
+                    <div>
+                        <input
+                        value={odd}
+                        type="text"
+                        placeholder="Odd"
+                        onChange={(e) => [setOdd(e.target.value), setError("")]}
+                        />
+                    </div>
+                </div>
+            </form>
+            <button>Alterar</button>
         </div>
     )
 }
--- fase2/frontend/src/components/views/RemoveGameView.js
+++ fase2/frontend/src/components/views/RemoveGameView.js
@@ -7,33 +7,35 @@ export const RemoveGameView = () => {
     const [error, setError] = useState("");
     
     return (
-        <div className='remove-game-container'> 
-            <div class="row">
-                <div class="col-25">
-                    <label for="sport">Desporto:</label>
+        <div className='remove-game-container'>             
+            <form action="action_page.php">
+                <div class="row">
+                    <div class="col-25">
+                        <label for="sport">Desporto:</label>
+                    </div>
+                    <div>
+                        <input
+                        value={sport}
+                        type="text"
+                        placeholder="Desporto"
+                        onChange={(e) => [setSport(e.target.value), setError("")]}
+                        />
+                    </div>                        
                 </div>
-                <div>
-                    <input
-                    value={sport}
-                    type="text"
-                    placeholder="Desporto"
-                    onChange={(e) => [setSport(e.target.value), setError("")]}
-                    />
-                </div>                        
-            </div>
-            <div class="row">
-                <div class="col-25">
-                    <label for="gameID">Game ID:</label>
+                <div class="row">
+                    <div class="col-25">
+                        <label for="gameID">Game ID:</label>
+                    </div>
+                    <div>
+                        <input
+                        value={gameID}
+                        type="text"
+                        placeholder="Game ID"
+                        onChange={(e) => [setGameID(e.target.value), setError("")]}
+                        />
+                    </div>                        
                 </div>
-                <div>
-                    <input
-                    value={gameID}
-                    type="text"
-                    placeholder="Game ID"
-                    onChange={(e) => [setGameID(e.target.value), setError("")]}
-                    />
-                </div>                        
-            </div>
+            </form>
             <button>Remover</button>
         </div>
     )
