017df853d36f39ea8cc5247b7748c3adc6d67933
Hugand <hugoasgomes@hotmail.com>
2022-12-04 17:59:21 +0000

    Implement bets history backend endpoint

--- fase2/backend/src/controller/bets.controller.js
+++ fase2/backend/src/controller/bets.controller.js
@@ -1,4 +1,31 @@
-const { Bet, MbWayPayment, CardPayment } = require('../model/db/model.db')
+const { Bet, MbWayPayment, CardPayment, Odd, Game } = require('../model/db/model.db')
+
+exports.getBetsHistory = async (req, res) => {
+    const userData = req.jwt
+    const dbBets = await Bet.findAll({
+        where: {
+            userId: userData.id
+        },
+        order: [    
+            ['createdAt', 'DESC'],
+        ],
+    })
+
+    const parsedBets = []
+    for(let i = 0; i < dbBets.length; i++) {
+        const bet = dbBets[i].dataValues
+        bet.odd = await Odd.findOne({ where: { id: bet.oddId }})
+        bet.game = await Game.findOne({ where: { id: bet.gameId }})
+
+        bet.odd = bet.odd.dataValues
+        bet.game = bet.game.dataValues
+
+
+        parsedBets.push(bet)
+    }
+
+    return res.status(200).json(parsedBets)
+}
 
 exports.placeBet = async (req, res) => {
     const {
--- fase2/backend/src/routes/bets.routes.js
+++ fase2/backend/src/routes/bets.routes.js
@@ -3,9 +3,16 @@ const betsRouter = express.Router()
 const betsController = require('../controller/bets.controller')
 const authController = require('../controller/auth.controller')
 
+// Place bet
 betsRouter.post('/',[
     authController.validateJWT,
     betsController.placeBet
+]) 
+
+// Get bets history
+betsRouter.get('/',[
+    authController.validateJWT,
+    betsController.getBetsHistory
 ]) 
 
 exports.betsRouter = betsRouter
